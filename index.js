144059916ac5b638018cc437be9f0c767f431a72
Ardavan Zareirad <ardavan2525@gmail.com>
2023-03-10 17:37:22 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,64 @@
+function shout(string) {
+    // todo
+  }
+
+  function shout(string) {
+    return string;
+  }
+
+  "Hello!".toUpperCase(); // 'HELLO!'
+
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    // todo
+  } 
+  function whisper(string) {
+    return string;
+  }
+
+  "Hello!".toLowerCase(); // 'HELLO!'
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    // todo
+  }
+  function logShout(string) {
+    console.log(string);
+  }
+
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+
+  function logWhisper(string) {
+    // todo
+  }
+  function logWhisper(string) {
+    console.log(string);
+  }
+
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  function sayHiToHeadphonedRoommate(string){
+    switch (string) {
+        case string.toLowerCase():
+            return "I can't hear you!";
+            case string.toUpperCase():
+                return "YES INDEED!";
+                case "Let's have dinner together!":
+                    return "I would love to!";
+    }
+  }
+
+  
+
+  
+ 
+  
